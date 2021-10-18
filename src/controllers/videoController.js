588f6163dae966ada2fca92122b710d6dbdf23fd
zzemoo <chaerim.dev@gmail.com>
2021-10-18 15:02:23 +0900

    create account

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({}).sort({createdAt:"desc"});
+  const videos = await Video.find({}).sort({ createdAt: "desc" });
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = async (req, res) => {
@@ -17,7 +17,7 @@ export const getEdit = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
   if (!video) {
-    return res.render("404", { pageTitle: "Video not found." });
+    return res.status(404).render("404", { pageTitle: "Video not found." });
   }
   return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
@@ -26,7 +26,7 @@ export const postEdit = async (req, res) => {
   const { title, description, hashtags } = req.body;
   const video = await Video.exists({ _id: id });
   if (!video) {
-    return res.render("404", { pageTitle: "Video not found." });
+    return res.status(404).render("404", { pageTitle: "Video not found." });
   }
   await Video.findByIdAndUpdate(id, {
     title,
@@ -50,7 +50,7 @@ export const postUpload = async (req, res) => {
     });
     return res.redirect("/");
   } catch (error) {
-    return res.render("upload", {
+    return res.status(400).render("upload", {
       pageTitle: "Upload Video",
       errorMessage: error._message,
     });
@@ -64,15 +64,15 @@ export const deleteVideo = async (req, res) => {
 };
 
 export const search = async (req, res) => {
-  const {keyword} = req.query;
-  let videos =[];
+  const { keyword } = req.query;
+  let videos = [];
   if (keyword) {
     videos = await Video.find({
       title: {
         $regex: new RegExp(keyword, "i"),
       },
-    })
+    });
     console.log(videos);
   }
-  return res.render("search", {pageTitle:"Search", videos});
-}
+  return res.render("search", { pageTitle: "Search", videos });
+};
